74dfd24fb09dd3133040c89ff37c95941d36fa1f
Alfonso Pauciello <9438351+FonzTech@users.noreply.github.com>
2024-11-24 22:42:03 +0100

    Improve vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,11 @@
 // vitest.setup.ts
 
 import { vi } from 'vitest';
+import {Vector3} from "three/src/math/Vector3";
 
 // Mock the WebGLRenderer
 export const mock_Three_WebGLRenderer_setSize = vi.fn();
+export const mock_Three_WebGLRenderer_render = vi.fn();
 export const mock_Three_PerspectiveCamera_updateProjectionMatrix = vi.fn();
 
 vi.mock('three', async () => {
@@ -14,10 +16,12 @@ vi.mock('three', async () => {
     ...actualThree, // Spread the actual Three.js module to keep everything else intact
     WebGLRenderer: vi.fn().mockImplementation(() => ({
       setSize: mock_Three_WebGLRenderer_setSize,
+      render: mock_Three_WebGLRenderer_render,
       domElement: document.createElement('canvas') // Mock the canvas element
     })),
     PerspectiveCamera: vi.fn().mockImplementation(() => ({
-      updateProjectionMatrix: mock_Three_PerspectiveCamera_updateProjectionMatrix
+      updateProjectionMatrix: mock_Three_PerspectiveCamera_updateProjectionMatrix,
+      position: new Vector3()
     }))
   };
 });
